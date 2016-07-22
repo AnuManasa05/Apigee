676ceb06cbcc7e925c0559aa6a98e2b68bcc6c4f
AnuManasa05 <anucse2k11@gmail.com>
2016-07-22 07:53:30 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-apigeetool deploynodeapp -u 'anucse2k11@gmail.com' -p 'AnuManasa18' -o anumanasa -e prod -n 'node2' -d . -m hello-world-server.js -b /node2 -R --verbose --debug
+apigeetool deploynodeapp -u 'spoola@gmail.com' -p 'Miracle@001' -o sreevidya -e prod -n 'node2' -d . -m hello-world-server.js -b /node2 -R --verbose --debug
 echo "You can now test out the API proxy by entering http://$org-$env.apigee.net/node2 in cURL or your browser"
